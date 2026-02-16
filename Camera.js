b00367fa2c0ccd8ef3ca1319713cd8adfa0366cd
Mira Saini <mirasaini1005@icloud.com>
2026-02-15 16:40:05 -0800

    added high walls and made char taller

--- Camera.js
+++ Camera.js
@@ -1,6 +1,6 @@
 class Camera{
     constructor(){
-        this.eye = new Vector3([0,0,3]);
+        this.eye = new Vector3([0,.4,3]);
         this.at = new Vector3([0,0,-100]);
         this.up = new Vector3([0,1,0]);
         this.groundLevel = .75; // y-coordinate of the ground plane
